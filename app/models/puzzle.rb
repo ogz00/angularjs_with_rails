618ed7458b87322e30947a373e0c329cbbba4b34
Oguzhan <oguzhan@halici.com.tr>
2016-09-29 15:56:10 +0300

    paperclip

--- app/models/puzzle.rb
+++ app/models/puzzle.rb
@@ -8,7 +8,7 @@ class Puzzle < ActiveRecord::Base
   has_many :comments, :dependent => :delete_all
 
   def as_json(options = {})
-    super(options.merge({except: [:answer, :timestamp, :updated_at, :created_at]}))
+    super(options.merge({except: [:answer, :timestamp, :updated_at, :created_at], methods: :image}))
   end
 
 
